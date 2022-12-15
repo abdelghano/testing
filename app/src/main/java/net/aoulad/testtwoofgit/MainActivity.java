beb04913b8919556b251c3132ee3d36cd811b9ad
abdelghani <aaouladlaarbi@gmail.com>
2022-12-15 09:48:03 +0100

    first project

---
+++ app/src/main/java/net/aoulad/testtwoofgit/MainActivity.java
@@ -0,0 +1,22 @@
+package net.aoulad.testtwoofgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+TextView text;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text  =findViewById(R.id.mytext);
+
+
+
+    }
+}
